bd333afc6554623785a3667332b8f7b14430bdab
Alexis Aguilar <alexmerona@gmail.com>
2022-06-26 13:00:51 -0400

    adding carousels, FIGURED OUT THE JS!!!!

--- js/additions.js
+++ js/additions.js
@@ -21,10 +21,12 @@ options.addEventListener('change', (event) => {
 // ~~~~ MULTI SLIDESHOWS
 // ===================================================================
 
-let slideIndex = [1,2]; // Chooses the indexes of array "slideId" (on line 25)
-let slideId = ["mySlides1", "mySlides2"] // Classed the members of each slideshow group with different CSS classes. Placed them in array
+let slideIndex = [1,2,3,4]; // Chooses the indexes of array "slideId" (on line 25)
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"] // Classed the members of each slideshow group with different CSS classes. Placed them in array
 showSlides(1, 0);
 showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
